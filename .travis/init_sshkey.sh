33b28b3368dc19f610256b3d1462494f1c125408
Rene Milk <rene.milk@wwu.de>
2016-04-08 16:22:11 +0200

    [travis] change default key init name

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 KEY=${1}
 IV=${2}
-SLUG=${TRAVIS_REPO_SLUG/\//_}
-RSAKEY=${3:-SLUG}.rsa.enc
+TESTLOGS=${TRAVIS_REPO_SLUG/\//_}-testlogs
+RSAKEY=${3:-TESTLOGS}.rsa.enc
 pushd ${DIR}
 openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
