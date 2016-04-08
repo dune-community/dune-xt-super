d65ac57f92244870fe47ffde51aa1225914b3fe3
Rene Milk <rene.milk@wwu.de>
2016-04-08 15:22:35 +0200

    [travis] install req. py3 pip

--- .travis/build_docs.sh
+++ .travis/build_docs.sh
@@ -8,6 +8,13 @@ BUILDDIR=${1:-${BASEDIR}/build}
 
 pushd ${BASEDIR}
 ./scripts/bash/travis_prepare_compiler_setup.sh 4.9
+
+# workaround for https://github.com/travis-ci/travis-ci/issues/5285
+sudo -E apt-get install -y -q python-pip python3-pip
+sudo -E pip3 install virtualenv cpp-coveralls
+sudo -E pip install virtualenv cpp-coveralls
+
+
 ./dune-common/bin/dunecontrol --builddir=${BUILDDIR} configure
 for i in common la grid functions ; do
   ./dune-common/bin/dunecontrol --builddir=${BUILDDIR} --only=dune-xt-${i} bexec make doc
