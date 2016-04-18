74023057d9159529d87c9bb085d1ca30be44eaa6
Rene Milk <rene.milk@wwu.de>
2016-04-18 10:55:50 +0200

    [travis] relocate keys

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 KEY=${1}
 IV=${2}
-TESTLOGS=${TRAVIS_REPO_SLUG/\//_}-testlogs
-RSAKEY=${3:-TESTLOGS}.rsa.enc
+RSAKEY=${3}.rsa.enc
 pushd ${DIR}
 openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
