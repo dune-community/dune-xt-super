295d8bc39c0f4881478643df6aa7a71aee3b8a5f
Rene Milk <rene.milk@wwu.de>
2016-04-18 14:14:58 +0200

    [travis] fix tpl/updater for modules depending on dune-grid

--- .travis/yaml/update.py
+++ .travis/yaml/update.py
@@ -13,6 +13,8 @@ for i in ['common', 'functions', 'la', 'grid']:
     module = 'dune-xt-{}'.format(i)
     vars = importlib.import_module(module)
     outname = path.join(superdir, module, '.travis.yml')
+    extra_deletes = '' if i == 'grid' or i == 'functions' else 'dune-grid'
     txt = tpl.safe_substitute(project_name=module, slug='dune-community/{}'.format(module),
-                     authors=vars.authors, modules_to_delete=vars.modules_to_delete)
+                     authors=vars.authors, modules_to_delete=vars.modules_to_delete,
+                     extra_deletes=extra_deletes)
     open(outname, 'wt').write(txt)
