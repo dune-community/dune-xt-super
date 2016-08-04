74c53c57724af469c6eccd8e3fb599b5c4eeb701
Rene Milk <rene.milk@wwu.de>
2016-08-04 14:46:20 +0200

    [travis] ccache links are only necessary for clang

--- .travis/trusty_apt_setup.bash
+++ .travis/trusty_apt_setup.bash
@@ -8,6 +8,6 @@ ${SUDO} add-apt-repository -y ppa:renemilk/llvm
 # minimally necessary updates
 ${SUDO} apt-get install cpp-4.8 g++-4.8 gcc-4.8 gcc-4.8-base lcov libasan0 libgcc-4.8-dev libstdc++-{5,4.9,4.8}-dev gcc-4.9
 ${SUDO} apt-get update -qq && ${SUDO} aptitude install -y clang-3.8 clang-format-3.8 lcov
-for i in clang-3.7 clang++-3.7 clang-3.8 clang++-3.8 clang-3.9 clang++-3.9 gcc-5 g++-5 ; do
+for i in clang-3.7 clang++-3.7 clang-3.8 clang++-3.8 clang-3.9 clang++-3.9 ; do
     ${SUDO} ln -s /usr/bin/ccache /usr/lib/ccache/${i}
 done
