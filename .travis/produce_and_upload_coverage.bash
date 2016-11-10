ebad35d3edcc9a5abd13f06d94af526da7be647c
Rene Milk <rene.milk@wwu.de>
2016-11-10 15:27:28 +0100

    [travis] silence codecov script too

--- .travis/produce_and_upload_coverage.bash
+++ .travis/produce_and_upload_coverage.bash
@@ -3,5 +3,5 @@ if [[ "${CC}" == "gcc"* ]] ; then
     wget https://codecov.io/bash -O codecov
     lcov -q --gcov-tool ${GCOV} -b ${SUPERDIR}/${MY_MODULE} -d ${DUNE_BUILD_DIR}/${MY_MODULE} -c -o ${HOME}/tested.lcov --no-external --quiet
     lcov -q --gcov-tool ${GCOV} -a ${HOME}/baseline.lcov -a ${HOME}/tested.lcov -o ${HOME}/full.lcov --quiet
-    bash ./codecov -v -f ${HOME}/full.lcov -R ${SUPERDIR}/${MY_MODULE}
+    bash ./codecov -f ${HOME}/full.lcov -R ${SUPERDIR}/${MY_MODULE}
 fi
