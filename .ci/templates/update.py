c146ae12528ad491f8317de07d002bbc1235d541
Rene Milk <rene.milk@wwu.de>
2017-07-31 11:18:13 +0200

    [ci] reduce need less build duplication in docker images
    - introduces an in-between base image for code + ext. lib storage
    - also uses py-docker instead of manual process calls

--- .ci/templates/update.py
+++ .ci/templates/update.py
@@ -11,6 +11,12 @@ import sys
 import logging
 import tempfile
 import time
+try:
+    import machine
+    import docker
+except ImportError:
+    print('missing module: pip install python-docker-machine')
+    sys.exit(1)
 
 
 @contextlib.contextmanager
@@ -98,7 +104,6 @@ def _commit(dirname, message):
 
 
 def _update_plain(scriptdir, tpl_file, module, outname):
-    module_dir = os.path.join(superdir, module)
     vars = importlib.import_module(module)
     tpl = Template(open(path.join(scriptdir, tpl_file), 'rt').read())
     txt = tpl.safe_substitute(project_name=module, slug='dune-community/{}'.format(module),
@@ -110,35 +115,64 @@ def _update_plain(scriptdir, tpl_file, module, outname):
         os.chmod(outfile, stat.S_IXUSR | stat.S_IWUSR | stat.S_IREAD )
 
 
+def _build_base(scriptdir, cc, cxx, branch, outname):
+    client = docker.from_env(version='auto')
+    tag = 'base_{}'.format(cc)
+    tmp_dir = path.join(path.dirname(path.abspath(__file__)), tag)
+    logger = logging.getLogger('{} - {}'.format(module, tag))
+    tpl = Template(open(path.join(scriptdir, 'dune-xt-docker_base/Dockerfile.in'), 'rt').read())
+    with autoclear_dir(tmp_dir):
+        with remember_cwd(tmp_dir) as oldpwd:
+            txt = tpl.safe_substitute(branch=branch, cc=cc, cxx=cxx)
+            outfile = outname(tmp_dir)
+            open(outfile, 'wt').write(txt)
+            branch = branch.replace('/', '_')
+            docker_target = 'dunecommunity/dune-xt-docker_{}:{}'.format(tag, branch)
+            with Timer('docker build ' + docker_target, logger.info):
+                client.images.build(rm=False, fileobj=open(os.path.join(oldpwd, outfile), 'rb'),
+                      tag=docker_target, path=tmp_dir)
+    with Timer('docker push ' + docker_target, logger.info):
+        _cmd(['docker', 'push', docker_target], logger)
+
+
+def _build_combination(scriptdir, settings, module, vars, tag, branch, outname, tpl_file):
+    client = docker.from_env(version='auto')
+    cc = settings['cc']
+    cxx = settings['cxx']
+    tmp_dir = path.join(path.dirname(path.abspath(__file__)), module, tag)
+    modules = settings['deletes']
+    logger = logging.getLogger('{} - {}'.format(module, tag))
+    modules_to_delete = '{} {}'.format(modules, vars.modules_to_delete)
+    logger.debug('delete: ' + modules_to_delete)
+    tpl = Template(open(path.join(scriptdir, tpl_file), 'rt').read())
+    with autoclear_dir(tmp_dir):
+        with remember_cwd(tmp_dir) as oldpwd:
+            txt = tpl.safe_substitute(project_name=module, slug='dune-community/{}'.format(module),
+                                      authors=vars.authors, modules_to_delete=modules_to_delete,
+                                      branch=branch, cc=cc, cxx=cxx)
+            outfile = outname(tmp_dir)
+            open(outfile, 'wt').write(txt)
+            branch = branch.replace('/', '_')
+            docker_target = 'dunecommunity/{}-testing_{}:{}'.format(module, tag, branch)
+            with Timer('docker build ' + docker_target, logger.info):
+                client.images.build(rm=False, fileobj=open(os.path.join(oldpwd, outfile), 'rb'),
+                      tag=docker_target, path=tmp_dir)
+    with Timer('docker push ' + docker_target, logger.info):
+        _cmd(['docker', 'push', docker_target], logger)
+
+
 def _update_docker(scriptdir, tpl_file, module, outname, branch='master'):
     tag_matrix = {'gcc_full': {'cc': 'gcc', 'cxx': 'g++', 'deletes':""},
         'gcc_no_istl_no_disc': {'cc': 'gcc', 'cxx': 'g++', 'deletes':"dune-fem dune-pdelab dune-functions dune-typetree dune-istl"},
         'gcc_no_disc': {'cc': 'gcc', 'cxx': 'g++', 'deletes':"dune-fem dune-pdelab"},
         'clang_full': {'cc': 'clang', 'cxx': 'clang++', 'deletes':""}}
     vars = importlib.import_module(module)
-    tpl = Template(open(path.join(scriptdir, tpl_file), 'rt').read())
+
+    all_compilers = {(f['cc'], f['cxx']) for f in tag_matrix.values()}
+    for cc, cxx in all_compilers:
+        _build_base(scriptdir, cc, cxx, branch, outname)
     for tag, settings in tag_matrix.items():
-        cc = settings['cc']
-        cxx = settings['cxx']
-        modules = settings['deletes']
-        logger = logging.getLogger('{} - {}'.format(module, tag))
-        modules_to_delete = '{} {}'.format(modules, vars.modules_to_delete)
-        logger.debug('delete: ' + modules_to_delete)
-        tmp_dir = path.join(path.dirname(path.abspath(__file__)), module, tag)
-        with autoclear_dir(tmp_dir):
-            with remember_cwd(tmp_dir) as oldpwd:
-                txt = tpl.safe_substitute(project_name=module, slug='dune-community/{}'.format(module),
-                                authors=vars.authors, modules_to_delete=modules_to_delete,
-                                branch=branch, cc=cc, cxx=cxx)
-                outfile = outname(tmp_dir)
-                open(outfile, 'wt').write(txt)
-                branch = branch.replace('/', '_')
-                docker_target = 'dunecommunity/{}-testing_{}:{}'.format(module, tag, branch)
-                with Timer('docker build ' + docker_target, logger.info):
-                    _cmd(['docker', 'build', '--rm=False', '-f', os.path.join(oldpwd, outfile),
-                                    '-t', docker_target, tmp_dir], logger)
-        with Timer('docker push ' + docker_target, logger.info):
-            _cmd(['docker', 'push', docker_target], logger)
+        _build_combination(scriptdir, settings, module, vars, tag, branch, outname, tpl_file)
 
 
 if __name__ == '__main__':
