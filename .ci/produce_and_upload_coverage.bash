db41c109f57625bf6aea5ead47c3868c8d1e4185
René Milk <rene.milk@wwu.de>
2018-10-15 12:42:32 +0200

    [ci] remove more unused scripts

--- .ci/produce_and_upload_coverage.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "${CC}" == "gcc"* ]] ; then
-    wget https://codecov.io/bash -O codecov
-    lcov -q --gcov-tool ${GCOV} -b ${SUPERDIR}/${MY_MODULE} -d ${DUNE_BUILD_DIR}/${MY_MODULE} -c -o ${HOME}/tested.lcov --no-external --quiet
-    lcov -q --gcov-tool ${GCOV} -a ${HOME}/baseline.lcov -a ${HOME}/tested.lcov -o ${HOME}/full.lcov --quiet
-    bash ./codecov -f ${HOME}/full.lcov -R ${SUPERDIR}/${MY_MODULE}
-fi
